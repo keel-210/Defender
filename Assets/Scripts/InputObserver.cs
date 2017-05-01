691244f7cb73382b00db19c8bd3b03ded19b5f02
keel-210 <crafters_touhou@yahoo.co.jp>
2017-05-02 01:50:46 +0900

    Def=Ver0.00
    For Android

---
+++ Assets/Scripts/InputObserver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputObserver : MonoBehaviour {
+
+	void Start ()
+    {
+        Screen.orientation = ScreenOrientation.AutoRotation;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        Screen.autorotateToPortraitUpsideDown = false;
+    }
+	
+	void Update ()
+    {
+		foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                pos = new Vector3(pos.x, pos.y, 0);
+                transform.position = pos;
+            }
+        }
+    }
+}
